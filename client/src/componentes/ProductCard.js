711947898e4da1915063ea169826c4fc9adc2531
Wanco0 <wencatala@outlook.com>
2020-10-06 16:11:05 -0300

    com para hacer pull

---
+++ client/src/componentes/ProductCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles/ProductCard.css';
+
+function ProductCard (params) {
+    return (
+      <div>
+      
+      </div>
+    ); 
+}
+
+export default ProductCard;
