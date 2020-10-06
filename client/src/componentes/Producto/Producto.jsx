5f831421c45eb074e07a07ee72fca83836790eb5
Hernan Chamorro <lc.hernanchamorro@gmail.com>
2020-10-06 12:08:42 -0300

    Prueba

--- client/src/componentes/Producto/Producto.jsx
+++ client/src/componentes/Producto/Producto.jsx
@@ -1,36 +1,22 @@
-import React from "react";
-import "./Producto.css";
+import React from 'react'
 
-function Producto({ titulo, descripcion, precio, cantidad }) {
-  return (
-    <div class="bg-gray-50">
-      <div class="max-w-screen-xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
-        <h2 class="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-          Ready to dive in?
-          <br />
-          <span class="text-indigo-600">Start your free trial today.</span>
-        </h2>
-        <div class="mt-8 flex lg:flex-shrink-0 lg:mt-0">
-          <div class="inline-flex rounded-md shadow">
-            <a
-              href="#"
-              class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-            >
-              Get started
-            </a>
-          </div>
-          <div class="ml-3 inline-flex rounded-md shadow">
-            <a
-              href="#"
-              class="inline-flex items-center justify-center px-5 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-indigo-600 bg-white hover:text-indigo-500 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out"
-            >
-              Learn more
-            </a>
-          </div>
+const Card = ({photo}) => {
+    return (
+        <div className='card'>
+            <img src={photo} alt="" className='w-full' />
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+                <p className="text-gray-700 text-base">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+                </p>
+            </div>
+            <div className="px-6 py-4">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
 
-export default Producto;
+export default Card
