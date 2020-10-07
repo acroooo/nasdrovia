1fdb236dbdce9c1b3b707605a873d8f73ae7ea31
Felipe Taboada <ftaboada@gmail.com>
2020-10-07 08:24:39 -0300

    funcionalidad filtrado catalogo

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Categoria from './componentes/Categoria/Categoria'
+import Categoria from "./componentes/Categoria/Categoria";
 
 function App() {
-
-  const src = 'https://cdn.pastemagazine.com/www/articles/ballast%20point%20sculpin.png';
-  const productos = [{ nombre: 'producto1', id: 0, photo: src, asd: "agreguen más cosas", categoria: "lager" }, { nombre: 'producto2', id: 1, photo: src, asd: "agreguen más cosas", categoria: "stout" }];
-  const categorias = ['Lager', 'Stout', 'Pilsner', 'Ipa', 'Red-Ale', 'Pale-Ale'];
   return (
     <div className="App">
-      <Categoria productosArr={productos} categorias={categorias} />
+      <Categoria />
     </div>
   );
 }
