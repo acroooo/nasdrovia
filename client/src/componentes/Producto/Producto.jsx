5ca6e4e0608fce401b26bf9be9cde43a7ad18f19
Hernan Chamorro <lc.hernanchamorro@gmail.com>
2020-10-07 11:54:06 -0300

    hardcode con objeto props

--- client/src/componentes/Producto/Producto.jsx
+++ client/src/componentes/Producto/Producto.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import "./Producto.css";
 
 const Producto = ({nombre, descripcion, precio, cantidad}) => {
+
+  const props = {
+    nombre: "Belgian Blonde Ale",
+    descripcion: "Linea Starke, cerveza de tonos rubios, sabores intensos, lista y fresca para disparar el lupulo de su cepa. Desde los alpes alemanes a la puerta de tu casa.",
+    precio: "$59",
+    cantidad: [1,6,12,24]
+  }
   return (
     <div className="text-gray-700 body-font overflow-hidden">
   <div className="container px-5 py-24 mx-auto">
@@ -9,7 +16,7 @@ const Producto = ({nombre, descripcion, precio, cantidad}) => {
       <img alt="ecommerce" className="lg:w-1/2 w-full lg:h-auto h-64 border rounded" src="https://d26lpennugtm8s.cloudfront.net/stores/001/173/096/products/belgian-blonde-ale-1024-frontal1-06da329b1d46f92c5115939085828981-640-0.jpg" />
       <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 mt-6 lg:mt-0 margen">
         <h2 className="text-sm title-font text-black-500 tracking-widest text-2xl">Starke</h2>
-        <h1 className="text-gray-900 text-5xl title-font font-medium mb-1">Belgian Blonde Ale</h1>
+        <h1 className="text-gray-900 text-5xl title-font font-medium mb-1">{props.nombre}</h1>
         <div className="flex mb-4">
           {/* <span className="flex items-center">
           <button className="border-2 border-gray-300 rounded-full w-6 h-6 focus:outline-none"></button>
@@ -35,7 +42,7 @@ const Producto = ({nombre, descripcion, precio, cantidad}) => {
             </a>
           </span> */}
         </div>
-        <p className="p">Linea Starke, cerveza de tonos rubios, sabores intensos, lista y fresca para disparar el lupulo de su cepa. Desde los alpes alemanes a la puerta de tu casa.</p>
+        <p className="p">{props.descripcion}</p>
         <div className="flex mt-6 items-center pb-5 border-b-2 border-gray-200 mb-5">
           <div className="flex">
             {/* <span className="mr-3">Cepa</span>
@@ -43,7 +50,7 @@ const Producto = ({nombre, descripcion, precio, cantidad}) => {
             <button className="border-2 border-gray-300 ml-1 bg-gray-700 rounded-full w-6 h-6 focus:outline-none"></button>
             <button className="border-2 border-gray-300 ml-1 bg-indigo-500 rounded-full w-6 h-6 focus:outline-none"></button> */}
           </div>
-          <div className="flex ml-6 cantidades">
+          <div className="flex ml-6">
             <span className="mr-56">Cantidad</span>
             <div className="selector">
               <select className="rounded selector border border-gray-400 py-2 focus:outline-none focus:border-indigo-500 text-base pl-3 pr-10">
@@ -61,7 +68,7 @@ const Producto = ({nombre, descripcion, precio, cantidad}) => {
           </div>
         </div>
         <div className="flex">
-          <span className="title-font font-medium text-2xl text-gray-900">$58.00</span>
+          <span className="title-font font-medium text-2xl text-gray-900">{props.precio}</span>
           <button className="agregar flex ml-auto text-white bg-red-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">Agregar</button>
           {/* <button className="rounded-full w-10 h-10 bg-gray-200 p-0 border-0 inline-flex items-center justify-center text-gray-500 ml-4">
             <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" className="w-5 h-5" viewBox="0 0 24 24">
