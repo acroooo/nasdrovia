0886465d2a1591054d1fe507b4b0f6aa9d8bcccf
Lismairy Sanchez <sanchezlismairy@gmail.com>
2020-10-07 20:02:23 -0300

    Ruta sin probar

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,21 +1,41 @@
 const express = require("express");
 // import all routers;
-const categorias = require("./categorias.js")
+const categorias = require("./categorias.js");
 const producto = require("./producto.js");
+const { Op } = require("sequelize");
 
 const app = express();
-
-const productRouter = require("./producto.js");
-const categoryRouter = require("./categoria.js");
-
-
-
-// load each router on a route
-// i.e: router.use('/auth', authRouter);
-// router.use('/auth', authRouter);
-
 
 app.use("/producto", producto);
 app.use("/producto/categorias", categorias);
 
+app.get("/search", (req, res) => {
+  const { query, q } = req.query;
+  const PrimerIndice = (q - 1) * 2;
+  const SegundoIndice = PrimerIndice + 2;
+  producto
+    .FindAll({
+      where: {
+        [Op.or]: [
+          { nombre: { [Op.iLike]: `%${query}%` } },
+          { descripcion: { [Op.iLike]: `%${query}%` } },
+        ],
+      },
+    })
+    .then((response) => {
+      if (response.length <= 0)
+        return res
+          .status(404)
+          .send("No se encontró ningún producto con ese nombre o descripción!");
+      let productos = response;
+      let pagina = productos.slice(PrimerIndice, SegundoIndice);
+      let resultado = {
+        PaginaActual: productos.length > 0 ? q : q - 1,
+        productos: pagina,
+        mas: pagina.length > 0 ? true : false,
+      };
+      return res.send(resultado);
+    })
+    .catch(() => res.status(400).send("Algo salió mal"));
+});
 module.exports = app;
