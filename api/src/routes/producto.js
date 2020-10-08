9f5facb01b32596605c9cd5f6be733c8532fd914
Hernan Chamorro <lc.hernanchamorro@gmail.com>
2020-10-08 01:13:41 -0300

    fix server por router

--- api/src/routes/producto.js
+++ api/src/routes/producto.js
@@ -10,5 +10,20 @@ router.get('/',(req, res, error) => {
 		})
 		.catch(error.message);
 });
-
-module.exports = router;
+router.post('/', async (req, res, next) => {
+	const {nombre, precio, stock, imagen, descripcion} = req.body;
+	console.log(req.body)
+	if(nombre && precio && stock && imagen && descripcion){
+		const nproduct = await Producto.create({
+			nombre: nombre,
+			precio: precio,
+			stock: stock,
+			imagen: imagen,
+			descripcion: descripcion
+		})
+		res.status(201).json(nproduct)
+	}else {
+		res.status(400).json({"Error":"Faltan parametros"})
+	}
+});
+module.exports = server;
