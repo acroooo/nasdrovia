ecc624b41c26477d8f67fae19427ce0b05c377a3
Lismairy Sanchez <sanchezlismairy@gmail.com>
2020-10-08 00:09:46 -0300

    Ruta lista

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -10,9 +10,7 @@ app.use("/producto", producto);
 app.use("/producto/categorias", categorias);
 
 app.get("/search", (req, res) => {
-  const { query, q } = req.query;
-  const PrimerIndice = (q - 1) * 2;
-  const SegundoIndice = PrimerIndice + 2;
+  const { query } = req.query;
   producto
     .FindAll({
       where: {
@@ -27,15 +25,8 @@ app.get("/search", (req, res) => {
         return res
           .status(404)
           .send("No se encontró ningún producto con ese nombre o descripción!");
-      let productos = response;
-      let pagina = productos.slice(PrimerIndice, SegundoIndice);
-      let resultado = {
-        PaginaActual: productos.length > 0 ? q : q - 1,
-        productos: pagina,
-        mas: pagina.length > 0 ? true : false,
-      };
-      return res.send(resultado);
     })
     .catch(() => res.status(400).send("Algo salió mal"));
 });
+
 module.exports = app;
