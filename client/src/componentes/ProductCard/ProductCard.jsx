8027a29e0fe42677537f576b74d22864f8e6cfbf
Hernan Chamorro <lc.hernanchamorro@gmail.com>
2020-10-07 21:31:39 -0300

    cambios package.json

--- client/src/componentes/ProductCard/ProductCard.jsx
+++ client/src/componentes/ProductCard/ProductCard.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 import {cartButton} from "../../Multimedia/Svgs";
 
-const Card = () => {
+const ProductCard = () => {
     return (
-      <div class="w-screen h-screen flex justify-center items-center ">
-        <div class="container mx-auto max-w-sm w-full p-4 sm:w-1/2 ">
-          <div class="card flex flex-col justify-center bg-white rounded-lg shadow-2xl shadow-inner border-2 border-ligthgray-900">
-            <div class="prod-title">
-              <p class="text-2xl text-gray-900 font-bold">Patagonia Amber Lager</p>
+      <div className="w-screen h-screen flex justify-center items-center ">
+        <div className="container mx-auto max-w-sm w-full p-4 sm:w-1/2 ">
+          <div className="card flex flex-col justify-center bg-white rounded-lg shadow-2xl shadow-inner border-2 border-ligthgray-900">
+            <div className="prod-title">
+              <p className="text-2xl text-gray-900 font-bold">Patagonia Amber Lager</p>
             </div>
-            <div class="prod-img">
+            <div className="prod-img">
               <img src="https://d26lpennugtm8s.cloudfront.net/stores/447/761/products/cerveza-patagonia-amber-lager-473ml1-04ed032175080a536915859457774813-1024-1024.jpg"
-                  class="w-full object-cover object-center" />
+                  className="w-full object-cover object-center" />
             </div>
-            <div class="prod-info grid gap-10">
-              <div class="flex flex-row  justify-around items-center text-gray-900">
-                <p class="font-bold text-xl">$65</p>
-                <button class="px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-600 hover:text-white border-2 border-gray-900 focus:outline-none">{cartButton}</button>
+            <div className="prod-info grid gap-10">
+              <div className="flex flex-row  justify-around items-center text-gray-900">
+                <p className="font-bold text-xl">$65</p>
+                <button className="px-6 py-2 transition ease-in duration-200 uppercase rounded-full hover:bg-gray-600 hover:text-white border-2 border-gray-900 focus:outline-none">{cartButton}</button>
               </div>
             </div>
           </div>
@@ -25,4 +25,4 @@ const Card = () => {
     )
 }
 
-export default Card
+export default ProductCard;
