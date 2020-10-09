1357f95c13aaa1c812fc2dc5ada37762066faebd
andres10sa <camand10sa@gmail.com>
2020-10-09 11:20:59 -0500

    cambios

--- client/src/componentes/FormularioCrud/FormularioCrud.jsx
+++ client/src/componentes/FormularioCrud/FormularioCrud.jsx
@@ -2,12 +2,15 @@ import React, { useState,useEffect } from 'react'
 import "./FormularioCrud.css"
 import { productos, ocultarFormulario, motrarFormulario, editarProducto } from './formulario';
 import Axios from 'axios';
-
-import ListaProductos from './ListaProductos/ListaProductos'
+import EncabezadoCrud from './EncabezadoCrud/EncabezadoCrud';
+import ListaProductos from './ListaProductos/ListaProductos';
+import TitulosFiltros from './TitulosFiltros/TitulosFiltros';
+import Crud from './Crud/Crud';
 
 const FormularioCrud = () => {
 
-    const [productoActual, setProductoActual] = useState({res:null, isLoaded:false});//producto que se va a postear
+    const [listadoProductos, setListadoProductos] = useState({res:null, isLoaded:false});//Listado de productos
+    const [productoActual,setProductoActual] = useState({})//Producto que se va a crear
     const [productoEditar, setProductoEditar] = useState({});//producto que se va a editar
     const [accion,setAccion]=useState('');//acción que se realiza actualmente
     const [categorias,setCategorias]=useState([]);//categorias del producto que se está creando o editando
@@ -19,18 +22,18 @@ const FormularioCrud = () => {
         imagen: '',
         descripcion: ''
     });
+     
 
     useEffect(()=>{
-    Axios.get('http://localhost:3001/producto').then(data =>{setProductoActual({res:data, isLoaded:true});
+    Axios.get('http://localhost:3001/producto').then(data =>{setListadoProductos({res:data, isLoaded:true});
         }).catch(error => 
         console.log(error));
     },[]);
-
-
+  
 
     //Crear nuevo producto o editar
     const handleChange = e => { 
-        accion ==='crear'&& setProductoActual({ ...productoActual, [e.target.name]: e.target.value,categorias:categorias }) //Crear producto y almacenarlo en el state
+        accion ==='crear'&& setListadoProductos({ ...listadoProductos, [e.target.name]: e.target.value,categorias:categorias }) //Crear producto y almacenarlo en el state
         accion ==='editar' && setProductoEditar({...productoEditar,[e.target.name]:e.target.value,categorias:categorias}) //editar producto actual y almacenarlo en el state
     };
 
@@ -44,15 +47,21 @@ const FormularioCrud = () => {
      let nuevas = categorias.filter(categoria=>categoria!==nombre);
      setCategorias(nuevas);
     }
-if(productoActual.isLoaded){
+    const datosCrearEditar = {
+        setCategorias,setProductoEditar,setAccion,setProductoActual,error,handleChange,inputValues,
+        categorias,eliminarCategoria,changeCategories
+    } 
+if(listadoProductos.isLoaded){
      return (
-
-         <div>
-        
-         <ListaProductos lista={productoActual.res} isLoaded={productoActual.isLoaded}/>
+         <div className='total'>
+             <div className='container general'>
+               <EncabezadoCrud setCategorias={setCategorias}setAccion={setAccion}/>
+               <TitulosFiltros/>
+               <ListaProductos lista={listadoProductos.res} isLoaded={listadoProductos.isLoaded}setProductoEditar={setProductoEditar} setAccion={setAccion}/>
+               <Crud datosCrearEditar={datosCrearEditar}/>
+             </div>
          </div>
-
-                    )
+)
                 }else {
                     return (
                         <div>
