2d401a35e0158885aa1d381123f4da8a60adebfb
Lismairy Sanchez <sanchezlismairy@gmail.com>
2020-10-12 15:28:16 -0300

    No anda

--- client/src/componentes/SearchBar/SearchBar.jsx
+++ client/src/componentes/SearchBar/SearchBar.jsx
@@ -31,9 +31,10 @@ export default function SearchBar() {
       <Navbar className="navbar-custom" variant="dark">
         <Navbar.Brand href="#home">
           <img
-            src="https://i.pinimg.com/originals/0f/72/14/0f721400c190ad9a138cd12d71694cdf.png"
+            src="https://i.pinimg.com/564x/ac/de/80/acde80ebc88d4dda88b10f7697cef890.jpg"
             alt="Logo"
             width="90px"
+            height="90px"
           />
         </Navbar.Brand>
         <Nav className="mr-auto">
@@ -61,8 +62,12 @@ export default function SearchBar() {
             </Button>
           </Link>
           <div className="carrito">{cartButton}</div>
-
-          <div className="usuario">{userButton}</div>
+          
+          <NavDropdown title={userButton} id="basic-nav-dropdown">
+            <NavDropdown.Item href="/formulario-categoria">Formulario Categoria</NavDropdown.Item>
+            <NavDropdown.Item href="/formulario-crud">Formulario Producto</NavDropdown.Item>
+          </NavDropdown>
+          <div className="usuario"></div>
         </Form>
       </Navbar>
     </Container>
