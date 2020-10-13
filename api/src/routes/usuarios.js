03b73bc544c2e2b3aaa6f303ca9a5d2320d75d91
HernanSsj <zed@pop-os.localdomain>
2020-10-13 18:07:34 -0300

    "arreglo" post

--- api/src/routes/usuarios.js
+++ api/src/routes/usuarios.js
@@ -5,28 +5,22 @@ const { Usuario} = require("../db.js");
 
 router.post("/", async (req, res, next) => {
   let { nombre, rol, email, contrasena} = req.body;
-  if(email){
-    let emailExistente = await Usuario.findOne({ where: { email: email } });
-    if(emailExistente){
-      res.status(400).json({ Error: "Email ya registrado" });
-    }else{
       if (nombre  && email  && contrasena) {
-        let nuevo_usuario = await Usuario.create({
-          name: nombre,
-          rol: rol,
-          email: email,
-          password: contrasena,
-        });
-        res.status(201).json(nuevo_usuario);
-      } else {
-        res.status(400).json({ Error: "Faltan parametros" });
+          let emailExistente = await Usuario.findOne({ where: { email: email} });
+            if(emailExistente){
+              res.status(400).json({ Error: "Email ya registrado" });
+             }else{
+               let nuevo_usuario = await Usuario.create({
+                  name: nombre,
+                  rol,
+                  email,
+                  password: contrasena,
+                });
+                res.status(201).json(nuevo_usuario);
+            }
+      }else{
+        res.status(400).json({ Error: "Faltan parametros todos son requeridos excepto el rol" });
       }
-    }
-  }else{
-    res.status(400).json({ Error: "Faltan parametros" });
-  }
-
-  
 });
 
 
