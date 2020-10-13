d90167720e87b956027e47a695400d897f333b68
Hernan Chamorro <lc.hernanchamorro@gmail.com>
2020-10-13 12:29:21 -0300

    BrowserRouter global

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom";
 import { Provider} from 'react-redux'
 import store from './store/index.js'
 import App from './App';
@@ -8,9 +9,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
+    <BrowserRouter>
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
   </Provider>,
   document.getElementById('root')
 );
