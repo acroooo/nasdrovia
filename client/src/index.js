5cf8e9b6b7d6be96e72ed665523a5843d24bf0d5
Hernan Chamorro <lc.hernanchamorro@gmail.com>
2020-10-13 12:29:10 -0300

    Provider y Store

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider} from 'react-redux'
+import store from './store/index.js'
 import App from './App';
 import './styles/reset.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
