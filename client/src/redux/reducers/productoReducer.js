4532cd3e8b507590c0f8144d41287238e41d107e
Hernan Chamorro <lc.hernanchamorro@gmail.com>
2020-10-15 08:52:27 -0300

    reducers1

--- client/src/redux/reducers/productoReducer.js
+++ client/src/redux/reducers/productoReducer.js
@@ -11,3 +11,29 @@ const initialState = {
     TodosLosProductos: [],
     Res: {}
 }
+export default function productoReducer(state = initialState, action) {
+    switch (action.type){
+        case GET_PRODUCTOS:
+            const productos = action.payload.map((p) => ({
+                id: p.id,
+                nombre: p.nombre,
+                precio: p.precio,
+                stock: p.stock,
+                imagen: p.imagen,
+                descripcion: p.descripcion,
+            }))
+            return {
+                ...state,
+                TodosLosProductos: productos,
+            };
+
+            case GET_PRODUCTO_DETALLE:
+                return;
+            case ADD_PRODUCT:
+                return {
+                    ...state,
+                    Res: action.payload,
+                };
+            case 
+    }
+}
