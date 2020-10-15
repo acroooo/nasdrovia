823245d365ea26352b74964e2b476bfc4c9a49e2
Alejandra <sanchezlismairy@gmail.com>
2020-10-15 17:14:20 -0300

    Aun no andan

--- api/src/routes/ordenes.js
+++ api/src/routes/ordenes.js
@@ -1,17 +1,17 @@
-const router = require("express").Router();
-const {Carrito, LineaDeOrden} = require("../db.js");
+// const router = require("express").Router();
+// const { Carrito, LineaDeOrden } = require("../db.js");
 
-router.get("/:id", (req, res)=>{
-    let order_id  = req.params.id;
-    const orden_unica = await Carrito.findOne({
-      where: { id: order_id },
-      //Traemos los datos del producto, precio, cantidad, etc
-      include: LineaDeOrden,
-    });
-    //existe esa orden?
-    if(orden_unica){
-      res.status(200).json(orden_unica)
-    }else{
-      res.status(200).json({error: "Orden no encontrada"})
-    }
-  })
+// router.get("/:id", async (req, res) => {
+//   let order_id = req.params.id;
+//   const orden_unica = Carrito.findOne({
+//     where: { id: order_id },
+//     //Traemos los datos del producto, precio, cantidad, etc
+//     include: LineaDeOrden,
+//   });
+//   //existe esa orden?
+//   if (orden_unica) {
+//     res.status(200).json(orden_unica);
+//   } else {
+//     res.status(200).json({ error: "Orden no encontrada" });
+//   }
+// });
