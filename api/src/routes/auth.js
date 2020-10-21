1f3982f9b9e96c46b35330f1a7738361d2b1b7cc
Alejandra <sanchezlismairy@gmail.com>
2020-10-20 23:30:52 -0300

    Passport implementado

--- api/src/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const router = require("express").Router();
-const {Usuario} = require("../db");
-const passport = require('passport');
-
-router.post('/login', passport.authenticate('local'),(req,res)=>{
-    res.status(201).json("Usuario se encuentra logueado");
-});
-
-module.exports=router;
